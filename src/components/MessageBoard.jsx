c21feb8671404361b455e62f263283eb360fedca
Panuwat <netpopo1150@outlook.com>
2024-01-28 18:40:21 +0700

    finish manipulate array

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,61 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [todos, setTodos] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const addList = (index) => {
+    index.preventDefault();
+    const newTodo = [...todos];
+    newTodo.push(message);
+    setTodos(newTodo);
+    console.log(todos);
+  };
+
+  const removeList = (index) => {
+    const newTodo = [...todos];
+    newTodo.splice(index, 1);
+    setTodos(newTodo);
+  };
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
-        <label>
-          <input
-            id="message-text"
-            name="message-text"
-            type="text"
-            placeholder="Enter message here"
-          />
-        </label>
-        <button className="submit-message-button">Submit</button>
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
+        <form>
+          <label>
+            <input
+              id="message-text"
+              name="message-text"
+              type="text"
+              placeholder="Enter message here"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </label>
+          <button className="submit-message-button" onClick={addList}>
+            Submit
+          </button>
+        </form>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        <ul>
+          <div >
+            {todos.map((todo, index) => {
+              return (
+                <li key={index} className="message">
+                  <h1>{todo}</h1>
+                  <button
+                    className="delete-button"
+                    onClick={(index) => removeList(index)}
+                  >
+                    x
+                  </button>
+                </li>
+              );
+            })}
+          </div>
+        </ul>
       </div>
     </div>
   );
